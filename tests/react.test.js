7bb4609a5ab9f05ff07f0c2c6c101d1113d6c71b
Neil Vodoor <nvodoor@gmail.com>
2018-10-29 19:40:59 -0700

    Fix tests

--- tests/react.test.js
+++ tests/react.test.js
@@ -42,7 +42,7 @@ test('Calendar Component should have working backDate method', () => {
   wrapper.setState({ date: '2018-10-24', dates: days.yearDates, dateIndex: days.yearIndexes });
   instance.backDate();
   expect(wrapper.state('date')).toBe('2018-10-23');
-})
+});
 
 test('Calendar Component should have working forwardDate method', () => {
   const wrapper = mount(<Calendar />);
@@ -52,7 +52,7 @@ test('Calendar Component should have working forwardDate method', () => {
   wrapper.setState({ date: '2018-10-24', dates: days.yearDates, dateIndex: days.yearIndexes });
   instance.forwardDate();
   expect(wrapper.state('date')).toBe('2018-10-25');
-})
+});
 
 test('Dates Component should have 7 dates within Calendar Component', () => {
   const wrapper = mount(<Calendar />);
