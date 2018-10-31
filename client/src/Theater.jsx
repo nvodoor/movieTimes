e9c4d226b8ad390559fc700005b366d159278046
Neil Vodoor <nvodoor@gmail.com>
2018-10-31 15:13:57 -0700

    Have sufficient code coverage

--- client/src/Theater.jsx
+++ client/src/Theater.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TheaterTime from './TheaterTime.jsx';
-import '../../public/styles.css';
+import './styles.css';
 
 
 const Theater = ({ theater, address, times }) => (
